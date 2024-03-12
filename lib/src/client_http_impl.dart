3f1b54104e64bb8641ee01e8365bcf2a8978e0a6
kevenmelo <keventjmelo@gmail.com>
2024-03-12 13:17:53 -0300

    fix

--- lib/src/client_http_impl.dart
+++ lib/src/client_http_impl.dart
@@ -1,29 +1,58 @@
 import 'package:client_http/client_http.dart';
 import 'package:http/http.dart' as http;
 
-class HttpClientImpl implements HttpClientInterface {
+class ClientImpl implements Client {
   @override
-  Future delete(Uri uri, {options, body}) {
-    return http.delete(uri, headers: options, body: body);
+  void close() {
+    http.Client().close();
   }
 
   @override
-  Future get(Uri uri, {options}) {
-    return http.get(uri, headers: options);
+  Future<Response> head(Uri url, {Map<String, String>? headers}) {
+    return http.head(url, headers: headers);
   }
 
   @override
-  Future post(Uri uri, {options, body}) {
-    return http.post(uri, headers: options, body: body);
+  Future<String> read(Uri url, {Map<String, String>? headers}) {
+    return http.read(url, headers: headers);
   }
 
   @override
-  Future put(Uri uri, {options, body}) {
-    return http.put(uri, headers: options, body: body);
+  Future<Uint8List> readBytes(Uri url, {Map<String, String>? headers}) {
+    return http.readBytes(url, headers: headers);
   }
 
   @override
-  Future patch(Uri uri, {options, body}) {
-    return http.patch(uri, headers: options, body: body);
+  Future<StreamedResponse> send(BaseRequest request) {
+    return http.Client().send(request);
+  }
+
+  @override
+  Future<Response> delete(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    return http.delete(url, headers: headers, body: body, encoding: encoding);
+  }
+
+  @override
+  Future<Response> get(Uri url, {Map<String, String>? headers}) {
+    return http.get(url, headers: headers);
+  }
+
+  @override
+  Future<Response> patch(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    return http.patch(url, headers: headers, body: body, encoding: encoding);
+  }
+
+  @override
+  Future<Response> post(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    return http.post(url, headers: headers, body: body, encoding: encoding);
+  }
+
+  @override
+  Future<Response> put(Uri url,
+      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
+    return http.put(url, headers: headers, body: body, encoding: encoding);
   }
 }
